99efe5ee767adb8c5df9b8632cd1081dedcd6eee
史 翔新 <x_shi@yumemi.co.jp>
2020-07-11 22:01:48 +0900

    Add dependencies for development

--- Package.swift
+++ Package.swift
@@ -10,11 +10,15 @@ let package = Package(
         .library(
             name: "DangerSwiftHammer",
             targets: ["DangerSwiftHammer"]),
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
+        // Dev dependencies
+        .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary.git", from: "1.0.0"), // dev
+        .package(url: "https://github.com/f-meloni/danger-swift-coverage.git", from: "1.0.0"), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,5 +29,6 @@ let package = Package(
         .testTarget(
             name: "DangerSwiftHammerTests",
             dependencies: ["DangerSwiftHammer"]),
+        .target(name: "DangerDependencies", dependencies: ["Danger", "DangerXCodeSummary", "DangerSwiftCoverage"]), // dev
     ]
 )
