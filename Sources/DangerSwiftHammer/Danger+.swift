e824eab8ca597698cc40b98b12dfa139566ea632
史 翔新 <x_shi@yumemi.co.jp>
2020-07-11 17:02:25 +0900

    Conform DangerDSL to DangerData protocol

---
+++ Sources/DangerSwiftHammer/Danger+.swift
@@ -0,0 +1,18 @@
+//
+//  Danger+.swift
+//  
+//
+//  Created by 史 翔新 on 2020/07/11.
+//
+
+import Danger
+
+extension DangerDSL: DangerData {
+    
+    public var hammer: DangerSwiftHammer<DangerDSL> {
+        
+        return DangerSwiftHammer(danger: self)
+        
+    }
+    
+}
