ff6ebdea27e976fc646ab85837fc658f9a458698
史 翔新 <x_shi@yumemi.co.jp>
2021-11-16 14:05:08 +0900

    Update tests

--- Tests/DangerSwiftHammerTests/DangerSwiftHammerTests.swift
+++ Tests/DangerSwiftHammerTests/DangerSwiftHammerTests.swift
@@ -5,13 +5,10 @@ final class DangerSwiftHammerTests: XCTestCase {
     
     func testDiffCommand() {
         
-        struct StubDanger: DangerDSLCompatible {
-            var baseBranch: String { "base_branch" }
-            func execShellCommand(_ command: String) -> String { "" }
-        }
+        let baseBranch: () -> String = { "base_branch" }
+        let shellCommand: (String) -> String = { _ in XCTFail(); return "" }
         
-        let danger = StubDanger()
-        let hammer = Hammer(danger: danger)
+        let hammer = Hammer(baseBranchResolver: baseBranch, shellCommandExecutor: shellCommand)
         let filename = "test.abc"
         
         let output = hammer.diffCommand(parsingFile: filename)
@@ -22,15 +19,10 @@ final class DangerSwiftHammerTests: XCTestCase {
         
     func testDiffPatch() {
         
-        struct StubDanger: DangerDSLCompatible {
-            var baseBranch: String { "base_branch" }
-            func execShellCommand(_ command: String) -> String {
-                return "'\(command)' executed"
-            }
-        }
+        let baseBranch: () -> String = { "base_branch" }
+        let shellCommand: (String) -> String = { "'\($0)' executed" }
         
-        let danger = StubDanger()
-        let hammer = Hammer(danger: danger)
+        let hammer = Hammer(baseBranchResolver: baseBranch, shellCommandExecutor: shellCommand)
         let filename = "test.abc"
         
         let output = hammer.diffPatch(for: filename)
@@ -41,44 +33,38 @@ final class DangerSwiftHammerTests: XCTestCase {
     
     func testDiffLines() {
         
-        struct StubDanger: DangerDSLCompatible {
-            
-            var baseBranch: String { "" }
-            
-            func execShellCommand(_ command: String) -> String {
-                return """
-                index 4888aaa..4c293b9 100644
-                --- a/Package.swift
-                +++ b/Package.swift
-                @@ -1,4 +1,4 @@
-                -// swift-tools-version:5.2
-                +// swift-tools-version:5.1
-                 // The swift-tools-version declares the minimum version of Swift required to build this package.
-                 
-                 import PackageDescription
-                @@ -14,13 +14,14 @@ let package = Package(
-                     dependencies: [
-                         // Dependencies declare other packages that this package depends on.
-                         // .package(url: /* package url */, from: "1.0.0"),
-                +        .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
-                     ],
-                     targets: [
-                         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-                         .target(
-                             name: "DangerSwiftHammer",
-                -            dependencies: []),
-                +            dependencies: ["Danger"]),
-                         .testTarget(
-                             name: "DangerSwiftHammerTests",
-                             dependencies: ["DangerSwiftHammer"]),
-                """
-            }
-            
+        let baseBranch: () -> String = { "" }
+        let shellCommand: (String) -> String = { _ in
+            return """
+            index 4888aaa..4c293b9 100644
+            --- a/Package.swift
+            +++ b/Package.swift
+            @@ -1,4 +1,4 @@
+            -// swift-tools-version:5.2
+            +// swift-tools-version:5.1
+             // The swift-tools-version declares the minimum version of Swift required to build this package.
+             
+             import PackageDescription
+            @@ -14,13 +14,14 @@ let package = Package(
+                 dependencies: [
+                     // Dependencies declare other packages that this package depends on.
+                     // .package(url: /* package url */, from: "1.0.0"),
+            +        .package(url: "https://github.com/danger/swift.git", from: "3.0.0"),
+                 ],
+                 targets: [
+                     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+                     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+                     .target(
+                         name: "DangerSwiftHammer",
+            -            dependencies: []),
+            +            dependencies: ["Danger"]),
+                     .testTarget(
+                         name: "DangerSwiftHammerTests",
+                         dependencies: ["DangerSwiftHammer"]),
+            """
         }
         
-        let danger = StubDanger()
-        let hammer = Hammer(danger: danger)
+        let hammer = Hammer(baseBranchResolver: baseBranch, shellCommandExecutor: shellCommand)
         
         let output = hammer.diffLines(in: "")
         let expectedOutput: (deletions: [String], additions: [String]) = (
